0bfdce453bddeb766dc19000a9b7d0cce75674c7
Nikita <myk.holitsyn@gmail.com>
2021-10-18 15:07:51 +0300

    all

---
+++ lesson1/UserMenu/src/UserMenu.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { UserDataContext } from './userData-context';
+
+class UserMenu extends React.Component {
+  render() {
+    console.log(this.context);
+    return (
+      <div className="menu">
+        <span className="menu__greeting">Hello, {this.context.name}</span>
+        <img alt="User Avatar" src={this.context.avatar_url} className="menu__avatar" />
+      </div>
+    );
+  }
+}
+
+UserMenu.contextType = UserDataContext;
+
+export default UserMenu;
