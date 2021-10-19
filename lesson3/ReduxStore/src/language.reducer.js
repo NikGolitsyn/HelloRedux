190b9a4f84bcd77a46abd98e73a9253cebdba93c
Nikita <myk.holitsyn@gmail.com>
2021-10-19 12:35:24 +0300

    all

--- lesson3/ReduxStore/src/language.reducer.js
+++ lesson3/ReduxStore/src/language.reducer.js
@@ -1,13 +1,9 @@
 import { SET_LANGUAGE } from './language.actions';
 
-const initialValue = {
-  language: 'en',
-};
-
-const languageReducer = (state = initialValue, action) => {
+const languageReducer = (state = 'en', action) => {
   switch (action.type) {
     case SET_LANGUAGE:
-      return { ...state, language: action.payload.language };
+      return action.payload.language;
 
     default:
       return state;
--- lesson3/ReduxStore/src/user.reducer.js
+++ lesson3/ReduxStore/src/user.reducer.js
@@ -1,16 +1,12 @@
 import { SET_USER, REMOVE_USER } from './user.actions';
 
-const initialState = {
-  user: null,
-};
-
 const userReducer = (state = null, action) => {
   switch (action.type) {
     case SET_USER:
-      return { ...state, user: action.payload.userData };
+      return action.payload.userData;
 
     case REMOVE_USER:
-      return { ...state, user: null };
+      return null;
 
     default:
       return state;
